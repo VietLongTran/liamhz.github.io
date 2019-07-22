ef16970c37267989ef1fb5d0315e18c057931fc9
Liam Hinzman <liamhinzman@l6-guest-mac.local>
2019-07-22 01:02:35 -0400

    Updated blog

---
+++ update_blog.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+hugo
+
+mv public/index.html public/blog.html
+mv public/index.xml public/blog.xml
+
+cp -r /public/* ./
+
+git add .
+git commit -m "Updated blog"
+git push origin master
